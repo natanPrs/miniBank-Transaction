660498d814565ebeea3d997f076a64f93f6a20cd
Natan Pires <natan.void@hotmail.com>
2025-03-25 21:38:11 -0300

    feat: add transaction controller

---
+++ src/main/kotlin/com/transaction/controllers/TransactionController.kt
@@ -0,0 +1,22 @@
+package com.transaction.controllers
+
+import com.transaction.dtos.TransactionDto
+import com.transaction.models.TransactionModel
+import com.transaction.services.TransactionService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/transactions")
+class TransactionController(private val transactionService: TransactionService) {
+
+    @PostMapping
+    fun createTransaction(@RequestBody transactionDto: TransactionDto): ResponseEntity<ResponseEntity<String>> {
+        val teste = transactionService.createTransaction(transactionDto)
+        return ResponseEntity(teste, HttpStatus.CREATED)
+    }
+}
