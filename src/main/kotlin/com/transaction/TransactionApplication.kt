ff44f8392640975cc94e5e54c53f7997f78d667d
Natan Pires <natan.void@hotmail.com>
2025-03-25 21:37:52 -0300

    chore: add feign

--- src/main/kotlin/com/transaction/TransactionApplication.kt
+++ src/main/kotlin/com/transaction/TransactionApplication.kt
@@ -2,7 +2,9 @@ package com.transaction
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients
 @SpringBootApplication
 class TransactionApplication
 
