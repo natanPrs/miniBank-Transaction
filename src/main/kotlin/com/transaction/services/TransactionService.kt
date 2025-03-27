b17253d1c9d771f6a3ec7e95b531acf246ef1878
Natan Pires <natan.void@hotmail.com>
2025-03-27 16:35:07 -0300

    feat: add saveTransaction and the actual behavior of the createTransaction

--- src/main/kotlin/com/transaction/services/TransactionService.kt
+++ src/main/kotlin/com/transaction/services/TransactionService.kt
@@ -1,18 +1,32 @@
 package com.transaction.services
 
+import com.transaction.dtos.ResponseTransactionDto
 import com.transaction.dtos.TransactionDto
-import com.transaction.mappers.toTransactionEntity
 import com.transaction.models.TransactionModel
 import com.transaction.repositories.TransactionRepository
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class TransactionService(
     private val transactionRepository: TransactionRepository,
     private val transactionServiceClient: TransactionServiceClient) {
 
-    fun createTransaction(transactionDto: TransactionDto): ResponseEntity<String> {
-        return transactionServiceClient.sendTransactionData(transactionDto)
+    fun createTransaction(transactionDto: TransactionDto): TransactionModel {
+        val response = transactionServiceClient.sendTransactionData(transactionDto)
+        return saveTransaction(response)
+    }
+
+    fun saveTransaction(responseTransactionDto: ResponseTransactionDto): TransactionModel {
+        val newTransaction = TransactionModel(
+            senderName = responseTransactionDto.senderName,
+            senderDocument = responseTransactionDto.senderDocument,
+            receiverName = responseTransactionDto.receiverName,
+            receiverDocument = responseTransactionDto.receiverDocument,
+            amount = responseTransactionDto.amount,
+            localStamp = LocalDateTime.now()
+        )
+
+        return transactionRepository.save(newTransaction)
     }
 }
