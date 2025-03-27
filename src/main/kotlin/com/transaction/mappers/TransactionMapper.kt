34ad212320c6bdd79b535e8360761523de628ae7
Natan Pires <natan.void@hotmail.com>
2025-03-27 16:37:45 -0300

    perf: temporarily discontinued

--- src/main/kotlin/com/transaction/mappers/TransactionMapper.kt
+++ src/main/kotlin/com/transaction/mappers/TransactionMapper.kt
@@ -5,10 +5,11 @@ import com.transaction.models.TransactionModel
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-fun TransactionDto.toTransactionEntity(transactionDto: TransactionDto): TransactionModel =
+/*fun TransactionDto.toTransactionEntity(transactionDto: TransactionDto): TransactionModel =
     TransactionModel(
         senderDocument = this.senderDocument,
         receiverDocument = this.receiverDocument,
         amount = this.amount,
         localStamp = LocalDateTime.now()
     )
+    */
